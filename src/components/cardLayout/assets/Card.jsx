2168eb742de9fbfa441d7e3493ba204c5a1161e9
Faruq Adebayo Atanda <atandafaruq25@gmail.com>
2022-08-10 13:47:13 +0100

    fixes

--- src/components/cardLayout/assets/Card.jsx
+++ src/components/cardLayout/assets/Card.jsx
@@ -59,10 +59,12 @@ const Card = () => {
     const printRef = useRef();
 
     const handleCard = async (option) => {
+
+
         // ---
         const digitalCard = printRef.current;
         // digitalCard.style.borderRadius = '0';
-        const canvas = await html2canvas(digitalCard);
+        const canvas = await html2canvas(digitalCard, { backgroundColor: null });
         const data = canvas.toDataURL('image/jpg');
         // ---
         switch (option) {
